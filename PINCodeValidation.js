d4a6000c94b5d69ad10f27eaf34166b02450c5fb
sharale34 <sourabhharale8000@gmail.com>
2020-11-14 23:39:00 +0530

    Added: Ability to validate mandatory part and start with abc

--- PINCodeValidation.js
+++ PINCodeValidation.js
@@ -1,17 +1,31 @@
 // The Postal Index Number Validation
 
 // Create a regex pattern to validate PIN code 400088
-function is_PINCode(str)
 {
- regexp = /^[1-9]{1}[0-9]{2}\s{0,1}[0-9]{3}$/;
-  
-        if (regexp.test(str))
-          {
+    function is_PINCode(Str) {
+        regexp = /^[1-9]{1}[0-9]{2}\s{0,1}[0-9]{3}$/;
+
+        if (regexp.test(Str)) {
             return true;
-          }
-        else
-          {
+        }
+        else {
             return false;
-          }
+        }
+    }
+    console.log("Is given PIN Code Valid - ", is_PINCode("400 088"));
 }
-console.log("Is given PIN Code Valid - ",is_PINCode("400 088"));
+
+//Validate Email address with a regex.
+{
+    function isValidEmailAddress(Str) {
+        regexp = /[a-zA-Z]$/;
+
+        if (regexp.test(Str)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    console.log("Is given Email Address Valid - ", isValidEmailAddress("abc"));
+}
