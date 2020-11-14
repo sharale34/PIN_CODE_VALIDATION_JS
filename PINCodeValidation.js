c4a5aa7f1b0584bb0699ea90e4db1f8b98f9379a
sharale34 <sourabhharale8000@gmail.com>
2020-11-14 18:12:00 +0530

    Added: Created a regex pattern to validate PIN code 400088

--- PINCodeValidation.js
+++ PINCodeValidation.js
@@ -1,2 +1,17 @@
 // The Postal Index Number Validation
 
+// Create a regex pattern to validate PIN code 400088
+function is_PINCode(str)
+{
+ regexp = /^[1-9][0-9]{5}$/;
+  
+        if (regexp.test(str))
+          {
+            return true;
+          }
+        else
+          {
+            return false;
+          }
+}
+console.log("Is given PIN Code Valid - ",is_PINCode("400088"));
